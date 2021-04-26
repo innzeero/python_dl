927eeaa1da7a44b3df7f5eea36b67ad82f82d4f4
innzeero <innzeero@gmail.com>
2021-04-26 23:58:42 +0900

    queue

--- tmp.py
+++ tmp.py
@@ -1,11 +1,12 @@
-# 1 stack , LIFO
+# Stack , LIFO
+# Last In First Out
 
 word = input("Input a word : ")
 
 word_list = list(word)
 
-for i in range(len(word_list)):
-    print(word_list.pop())
+# for i in range(len(word_list)):
+#     print(word_list.pop())
 
 # Input a word : stack
 # k
@@ -13,3 +14,11 @@ for i in range(len(word_list)):
 # a
 # t
 # s
+
+# Queue, FIFO
+# First In First Out
+# LIFO <-> FIFO
+# pop(0)
+
+for i in range(len(word_list)):
+    print(word_list.pop(0))
